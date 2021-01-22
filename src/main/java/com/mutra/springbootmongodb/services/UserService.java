b96290517a57198a7b790cfade7b6ab6db0b27ee
Mário Rabelo <mutrano@gmail.com>
2021-01-22 17:59:26 -0300

    User findById and exception handling in case it's not found

--- src/main/java/com/mutra/springbootmongodb/services/UserService.java
+++ src/main/java/com/mutra/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mutra.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mutra.springbootmongodb.domain.User;
 import com.mutra.springbootmongodb.repository.UserRepository;
+import com.mutra.springbootmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		List<User> users = repository.findAll();
 		return users;
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow( () -> new ObjectNotFoundException("Object not found"));
 	}
 }
