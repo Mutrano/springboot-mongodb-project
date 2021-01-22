2495c655b429629e60d07505be607db6f2a48c97
Mário Rabelo <mutrano@gmail.com>
2021-01-22 18:13:22 -0300

    add User Insert

--- src/main/java/com/mutra/springbootmongodb/services/UserService.java
+++ src/main/java/com/mutra/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mutra.springbootmongodb.domain.User;
+import com.mutra.springbootmongodb.dto.UserDTO;
 import com.mutra.springbootmongodb.repository.UserRepository;
 import com.mutra.springbootmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow( () -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
