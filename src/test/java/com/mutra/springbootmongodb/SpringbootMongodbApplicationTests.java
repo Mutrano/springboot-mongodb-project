bada412ccec127a8edc9f1f856bdb20056e2aaba
Mário Rabelo <mutrano@gmail.com>
2021-01-22 12:12:07 -0300

    Started project

---
+++ src/test/java/com/mutra/springbootmongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutra.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
