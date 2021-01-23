6c5783457ba91b063127a3f9cef0cd6086ee7f42
Mário Rabelo <mutrano@gmail.com>
2021-01-23 10:08:22 -0300

    add Post entity with nested User

--- src/main/java/com/mutra/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/mutra/springbootmongodb/config/Instantiation.java
@@ -1,12 +1,18 @@
 package com.mutra.springbootmongodb.config;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Arrays;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mutra.springbootmongodb.domain.Post;
 import com.mutra.springbootmongodb.domain.User;
+import com.mutra.springbootmongodb.repository.PostRepository;
 import com.mutra.springbootmongodb.repository.UserRepository;
 
 @Configuration
@@ -15,14 +21,24 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		userRepository.deleteAll();
+		DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(Locale.UK);
 		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 		User maria= new User(null, "Maria Brown", "maria@gmail.com");
 		User alex= new User(null, "Alex Green", "alex@gmail.com");
 		User bob= new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	
+		Post post1 = new Post(null,LocalDate.parse("21/03/2018", dtf), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",maria);
+		Post post2 = new Post(null,LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acordei feliz hoje!",maria);
+		postRepository.saveAll(Arrays.asList(post1,post2));
+		
 	}
 }
