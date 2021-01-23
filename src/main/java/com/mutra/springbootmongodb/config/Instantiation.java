6a3989fe83568691b1e85e6b5c33327725137b39
Mário Rabelo <mutrano@gmail.com>
2021-01-23 11:38:15 -0300

    add Post Comments

--- src/main/java/com/mutra/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/mutra/springbootmongodb/config/Instantiation.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import com.mutra.springbootmongodb.domain.Post;
 import com.mutra.springbootmongodb.domain.User;
 import com.mutra.springbootmongodb.dto.AuthorDTO;
+import com.mutra.springbootmongodb.dto.CommentDTO;
 import com.mutra.springbootmongodb.repository.PostRepository;
 import com.mutra.springbootmongodb.repository.UserRepository;
 
@@ -39,8 +40,18 @@ public class Instantiation implements CommandLineRunner {
 	
 		Post post1 = new Post(null,LocalDate.parse("21/03/2018", dtf), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null,LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!",LocalDate.parse("21/03/2018", dtf) , new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite",LocalDate.parse("22/03/2018", dtf) , new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",LocalDate.parse("23/03/2018", dtf) , new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		
+		
 	}
 }
