cf6be9d392d17d8b680eca2d0ba25d5f54bec052
Mário Rabelo <mutrano@gmail.com>
2021-01-23 11:11:24 -0300

    Endpoint for User Posts

--- src/main/java/com/mutra/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/mutra/springbootmongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mutra.springbootmongodb.domain.Post;
 import com.mutra.springbootmongodb.domain.User;
 import com.mutra.springbootmongodb.dto.UserDTO;
 import com.mutra.springbootmongodb.services.UserService;
@@ -62,5 +63,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
